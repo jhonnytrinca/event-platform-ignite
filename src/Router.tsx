e03befcc438e021f17b08e168b7d906a6edf2f81
Jhonny Trinca <jhonny.trinca@hotmail.com>
2022-06-24 12:12:44 -0300

    feat: create subscribe page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import { MainPage } from './pages/MainPage';
+import { Subscribe } from './pages/Subscribe';
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<>Teste</>} />
+      <Route path='/' element={<Subscribe />} />
       <Route path='/event' element={<MainPage />} />
       <Route path='/event/lesson/:slug' element={<MainPage />} />
     </Routes>
