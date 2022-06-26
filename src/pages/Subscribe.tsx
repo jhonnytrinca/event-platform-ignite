9e9e879bdcb42620baab405b7630c7e6ec69fd66
Jhonny Trinca <jhonny.trinca@hotmail.com>
2022-06-26 14:29:56 -0300

    feat: subscribe page mobile

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -24,21 +24,21 @@ export const Subscribe = () => {
 
   return (
     <div className='min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center'>
-      <div className='w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto'>
-        <div className='max-w-[640px]'>
+      <div className='w-full max-w-[1100px] flex flex-col md:flex-row items-center justify-between mt-10 md:mt-20 mx-auto'>
+        <div className='max-w-[640px] flex flex-col md:block items-center p-4 md:p-0'>
           <Logo />
-          <h1 className='mt-8 text-[2.5rem] leading-tight'>
+          <h1 className='mt-8 text-3xl md:text-[2.5rem] leading-tight text-center md:text-start'>
             Construa uma{' '}
             <strong className='text-blue-500'>aplicação completa</strong>, do
             zero, com <strong className='text-blue-500'>React JS</strong>
           </h1>
-          <p className='mt-4 text-gray-200 leading-relaxed'>
+          <p className='mt-4 text-gray-200 leading-relaxed text-center md:text-start text-sm md:text-base'>
             Em apenas uma semana você vai dominar na prática uma das tecnologias
             mais utilizadas e com alta demanda para acessar as melhores
             oportunidades do mercado.
           </p>
         </div>
-        <div className='p-8 bg-gray-700 border border-gray-500 rounded'>
+        <div className='w-full md:w-2/6 p-8 bg-gray-700 border border-gray-500 rounded'>
           <strong className='text-2xl mb-6 block'>
             Inscreva-se gratuitamente
           </strong>
